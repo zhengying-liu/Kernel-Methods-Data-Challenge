b8d48ae660dfeb89b003be64f6097d0018af5375
marioyc <ycmario@gmail.com>
2017-02-27 13:44:02 +0100

    complete fit function

--- cross_entropy_classifier.py
+++ cross_entropy_classifier.py
@@ -7,27 +7,46 @@ import numpy
 class CrossEntropyClassifier:
     """
     nclasses: number of classes (assumed between 0 and nclasses - 1)
-    W: projection matrix of size dimension of data x nclasses
+    W: projection matrix of size (dimension of data) x nclasses
     """
 
     def __init__(self, nclasses):
         self.nclasses = nclasses
+        self.W = None
 
-    def _calc_loss(X, y):
+    def _calc_loss(self, X, y):
         n = X.shape[0]
         loss = 0.0
 
-        for i in range(0,n):
-            p = numpy.dot(X[i, :], self.W[:, y[i]])
-            loss += -numpy.log(p)
+        P = numpy.dot(X, self.W)
+        P = numpy.exp(P)
+        sumP = numpy.sum(P, axis=1)
 
-        return loss / N
+        for i in range(n):
+            loss += -numpy.log(P[i, y[i]] / sumP[i])
 
-    def _calc_gradient(X, y1hot):
-        # TODO
-        pass
+        return loss / n
+
+    def _calc_gradient(self, X, y):
+        n,d = X.shape
+
+        P = numpy.dot(X, self.W)
+        P = numpy.exp(P)
+        sumP = numpy.sum(P, axis=1)
+
+        gradW = numpy.zeros(self.W.shape)
+        for i in range(n):
+            P[i,:] = P[i,:] / sumP[i]
+            for j in range(self.nclasses):
+                c = P[i,j] / P[i, y[i]]
+                if j == y[i]:
+                    c -= 1 / P[i, y[i]]
+                gradW[:, j] += c * X[i, :].T
+
+        return gradW
 
     """
+    X: matrix of size (number of data samples) x (dimension of data)
     iterations: number of gradient descent steps
     lr: fixed learning rate
     """
@@ -38,22 +57,25 @@ class CrossEntropyClassifier:
 
         n, d = X.shape
 
-        y1hot = numpy.zeros((n, self.nclasses))
-        for i in range(n):
-            y1hot[i, y[i]] = 1
-
         self.W = numpy.zeros((d, self.nclasses))
         history = {
-            'loss': [_calc_loss(X, y)]
+            'loss': [self._calc_loss(X, y)]
         }
 
         for it in range(iterations):
-            grad = _calc_gradient(X, y2)
-            self.W -= lr * grad
-            history['loss'].append(_calc_loss(X, y))
+            gradW = self._calc_gradient(X, y)
+            self.W -= lr * gradW
+            history['loss'].append(self._calc_loss(X, y))
 
         return history
 
     def predict(self, X):
         # TODO
         pass
+
+if __name__ == '__main__':
+    model = CrossEntropyClassifier(2)
+    X = numpy.array([[1,0], [-1,0]])
+    y = numpy.array([0,1])
+    history = model.fit(X,y)
+    print(history)
