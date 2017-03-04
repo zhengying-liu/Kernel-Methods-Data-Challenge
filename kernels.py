1d6d025eb8bbaef5f6cf01b648a3639d13d148d6
marioyc <ycmario@gmail.com>
2017-03-04 18:05:24 +0100

    add some more kernels

--- kernels.py
+++ kernels.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 import numpy
 
 def build_K(X, K_function):
@@ -5,16 +6,13 @@ def build_K(X, K_function):
     n = X.shape[0]
     K = numpy.zeros((n, n))
 
-    for i in range(n):
+    for i in tqdm(range(n)):
         for j in range(n):
             K[i, j] = K_function.calc(X[i, :], X[j, :])
 
     return K
 
 class LinearKernel:
-    def __init__(self):
-        pass
-
     def calc(self, x, y):
         return numpy.dot(x, y)
 
@@ -24,3 +22,24 @@ class GaussianKernel:
 
     def calc(self, x, y):
         return numpy.exp(-numpy.linalg.norm(x - y) ** 2 / (2 * self.sigma ** 2))
+
+class HistogramIntersectionKernel:
+    def __init__(self, beta):
+        self.beta = beta
+
+    def calc(self, x, y):
+        return numpy.sum(numpy.minimum(x ** self.beta, y ** self.beta))
+
+class LaplacianRBFKernel:
+    def __init__(self, sigma):
+        self.sigma = sigma
+
+    def calc(self, x, y):
+        return numpy.exp(-numpy.sum(numpy.abs(x - y)) / self.sigma**2)
+
+class SublinearRBFKernel:
+    def __init__(self, sigma):
+        self.sigma = sigma
+
+    def calc(self, x, y):
+        return numpy.exp(-numpy.sum(numpy.abs(x - y))**0.5 / self.sigma**2)
