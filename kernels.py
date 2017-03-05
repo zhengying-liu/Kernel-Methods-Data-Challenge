a208027dbe5013d443550d529a443ae65a99d522
marioyc <ycmario@gmail.com>
2017-03-05 23:38:27 +0100

    use optimized version when available

--- kernels.py
+++ kernels.py
@@ -1,20 +1,26 @@
 from tqdm import tqdm
 import numpy
 
-def build_K(X, K_function):
-    print("Buliding kernel matrix")
-    n = X.shape[0]
-    K = numpy.zeros((n, n))
+def build_K(X, kernel):
+    print("Building kernel matrix")
 
-    for i in tqdm(range(n)):
-        for j in range(n):
-            K[i, j] = K_function.calc(X[i, :], X[j, :])
+    if hasattr(kernel, "build_K") and callable(kernel.build_K):
+        return kernel.build_K(X, X)
+    else:
+        n = X.shape[0]
+        K = numpy.zeros((n, n))
+        for i in tqdm(range(n)):
+            for j in range(n):
+                K[i, j] = kernel.calc(X[i, :], X[j, :])
 
     return K
 
 class LinearKernel:
     def calc(self, x, y):
         return numpy.dot(x, y)
+
+    def build_K(self, X, Y):
+        return numpy.dot(X, Y.T)
 
 class GaussianKernel:
     def __init__(self, sigma):
@@ -28,7 +34,7 @@ class GaussianKernel:
         m = Y.shape[0]
         K = numpy.zeros((n, m))
 
-        for i in range(m):
+        for i in tqdm(range(m)):
             K[:, i] = numpy.linalg.norm(X - Y[i, :], axis=1) ** 2
         K /= 2 * self.sigma ** 2
         return numpy.exp(-K)
@@ -48,7 +54,7 @@ class GaussianKernelForAngle:
         Y2 = numpy.concatenate((numpy.sin(Y),numpy.cos(Y)), axis=1)
         K = numpy.zeros((n, m))
 
-        for i in range(m):
+        for i in tqdm(range(m)):
             K[:, i] = numpy.linalg.norm(X2 - Y2[i, :], axis=1) ** 2
         K /= 2 * self.sigma ** 2
         return numpy.exp(-K)
@@ -66,6 +72,16 @@ class LaplacianRBFKernel:
 
     def calc(self, x, y):
         return numpy.exp(-numpy.sum(numpy.abs(x - y)) / self.sigma**2)
+
+    def build_K(self, X, Y):
+        n = X.shape[0]
+        m = Y.shape[0]
+        K = numpy.zeros((n, m))
+
+        for i in tqdm(range(m)):
+            K[:, i] = numpy.sum(numpy.abs(X - Y[i, :]), axis=1)
+        K /= self.sigma ** 2
+        return numpy.exp(-K)
 
 class SublinearRBFKernel:
     def __init__(self, sigma):
