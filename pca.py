cb69545fd223a03e3a0c034b85d5ec0a93989145
ascane <mandy00724@gmail.com>
2017-03-09 14:36:44 +0100

    Fix pca. The size of every feature is reduced to the number of components.

--- pca.py
+++ pca.py
@@ -2,7 +2,6 @@ import numpy
 
 def pca(X, components):
     assert components > 0 and X.shape[1] >= components
-    U, s, Vt = numpy.linalg.svd(numpy.matrix(X), full_matrices=False)
-    V = Vt.T
+    U, s, _ = numpy.linalg.svd(numpy.matrix(X), full_matrices=False)
     S = numpy.diag(s)
-    return numpy.array(numpy.dot(U[:, :components], numpy.dot(S[:components, :components], V[:,:components].T)))
+    return numpy.array(numpy.dot(U[:, :components], S[:components, :components]))
