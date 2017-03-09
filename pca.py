75b8ef4b53f727c47f48272db8775255ceaf4990
ascane <mandy00724@gmail.com>
2017-03-09 15:24:31 +0100

    Change full_matrices to False in svd in pca to avoid memory error. Run big data.

--- pca.py
+++ pca.py
@@ -2,7 +2,7 @@ import numpy
 
 def pca(X, components):
     assert components > 0 and X.shape[1] >= components
-    U, s, Vt = numpy.linalg.svd(numpy.matrix(X), full_matrices=True)
+    U, s, Vt = numpy.linalg.svd(numpy.matrix(X), full_matrices=False)
     V = Vt.T
     S = numpy.diag(s)
     return numpy.array(numpy.dot(U[:, :components], S[:components, :components])), V[:, :components]
