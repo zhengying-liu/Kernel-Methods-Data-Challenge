4d13753b35966cf43c85edc259451348f830f2eb
ascane <mandy00724@gmail.com>
2017-03-12 15:52:04 +0100

    Minor change for consistency.

--- ensemble.py
+++ ensemble.py
@@ -1,17 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 10 16:43:07 2017
-
-@author: Evariste
-"""
-
+import numpy
 import os
 import pandas as pd
-import numpy as np
 
 def one_hot(i, n_classes=10):
-    return np.eye(n_classes)[i]
+    return numpy.eye(n_classes)[i]
 
 def ensemble_from_csv(directory="ensemble/", max_nb_models=3, save_name="ensemble.csv"):
     ensemble = []
@@ -26,7 +18,7 @@ def ensemble_from_csv(directory="ensemble/", max_nb_models=3, save_name="ensembl
                     break
     res = ensemble[0].copy()
     for index, row in res.iterrows():
-        sum_vec = np.zeros(10)
+        sum_vec = numpy.zeros(10)
         for df in ensemble:
             sum_vec += one_hot(df.loc[index].Prediction)
         row.Prediction = sum_vec.argmax()
